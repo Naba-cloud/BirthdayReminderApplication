6402e9ceb93603a8de5559f68cc35d06ff8c676e
Naba Zehra <59452017+Naba-cloud@users.noreply.github.com>
2021-06-06 19:12:38 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import React from "react";
+import ReactDom from "react-dom";
+import Form from "./Form";
+export default function App() {
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
+}
---
+++ src/Popup.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Popup = () => {
+  return (
+    <>
+      <h1>Birthday Added Succesfully </h1>
+    </>
+  );
+};
+export default Popup;
